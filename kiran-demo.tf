6c927d79a03c6b6700c83c648575397123351e88
kvasire <32622781+kvasire@users.noreply.github.com>
2017-10-09 21:57:16 -0400

    Delete kiran-demo.tf

--- kiran-demo.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-access_key= "AKIAJCE2WPJLQEQOLBXA"
-secret_key="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"
-region = "us-east-1"
-}
-resource "aws_instance" "Jenkins" {
-ami = "ami-8c1be5f6"
-instance_type = "t2.micro"
-}
